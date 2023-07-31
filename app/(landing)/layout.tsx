db18303e4c94f59ca28124c8df807b0ceb1e1764
szymonisokay <szymonwalach@gmail.com>
2023-07-31 22:29:35 +0200

    initialize clerk auth and modify styling

---
+++ app/(landing)/layout.tsx
@@ -0,0 +1,9 @@
+const LandingLayout = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<div className='flex flex-col items-center justify-center h-full'>
+			{children}
+		</div>
+	)
+}
+
+export default LandingLayout
