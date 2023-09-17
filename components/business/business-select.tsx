75def2679f0188106f6944ef393f935c6cfb2223
szymonisokay <szymonwalach@gmail.com>
2023-09-17 17:28:13 +0200

    refactor business select and add tooltip

--- components/business/business-select.tsx
+++ components/business/business-select.tsx
@@ -1,14 +1,27 @@
 'use client'
 
-import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
-} from '@/components/ui/select'
 import { Business } from '@prisma/client'
+import { CheckIcon, ChevronsUpDownIcon, PlusCircleIcon } from 'lucide-react'
 import { useParams } from 'next/navigation'
+
+import { Button } from '@/components/ui/button'
+import {
+	Command,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+	CommandList,
+	CommandSeparator,
+} from '@/components/ui/command'
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from '@/components/ui/popover'
+import { cn } from '@/lib/utils'
+import { useState } from 'react'
+import { TooltipHover } from '../tooltip-hover'
 
 type Props = {
 	businesses: Business[]
@@ -16,26 +29,75 @@ type Props = {
 
 export const BusinessSelect = ({ businesses }: Props) => {
 	const params = useParams()
+	const [open, setOpen] = useState<boolean>(false)
 
 	const selectedBusiness = businesses.find(
 		(business) => business.id === params.businessId
 	)
 
 	return (
-		<Select value={params.businessId as string}>
-			<SelectTrigger
-				title={selectedBusiness?.name}
-				className='w-full business-select'
+		<Popover open={open} onOpenChange={setOpen}>
+			<PopoverTrigger asChild>
+				<Button
+					role='combobox'
+					variant='outline'
+					className='justify-between w-full gap-x-2'
+				>
+					<TooltipHover content={selectedBusiness?.name}>
+						<span className='truncate'>
+							{selectedBusiness?.name}
+						</span>
+					</TooltipHover>
+					<ChevronsUpDownIcon className='w-4 h-4 text-muted-foreground shrink-0' />
+				</Button>
+			</PopoverTrigger>
+			<PopoverContent
+				align='start'
+				className='dark:bg-[#09090b] p-0 w-[218px]'
 			>
-				<SelectValue placeholder='Select business' />
-			</SelectTrigger>
-			<SelectContent className='max-h-[300px]'>
-				{businesses.map((business) => (
-					<SelectItem key={business.id} value={business.id}>
-						{business.name}
-					</SelectItem>
-				))}
-			</SelectContent>
-		</Select>
+				<Command className='dark:bg-[#09090b]'>
+					<CommandList>
+						<CommandInput placeholder='Search for a business' />
+						<CommandEmpty className='py-2'>
+							No business found.
+						</CommandEmpty>
+						<CommandGroup heading='Business'>
+							{businesses.map((business) => (
+								<CommandItem
+									key={business.id}
+									className={cn(
+										'relative pl-8 cursor-pointer',
+										selectedBusiness?.id === business.id &&
+											'bg-slate-100 dark:bg-[#040405]'
+									)}
+								>
+									<CheckIcon className='absolute w-4 h-4 left-2 shrink-0' />
+									<TooltipHover content={business.name}>
+										<span className='truncate'>
+											{business.name}
+										</span>
+									</TooltipHover>
+								</CommandItem>
+							))}
+						</CommandGroup>
+						<CommandSeparator />
+					</CommandList>
+					<CommandList>
+						<CommandGroup>
+							<CommandItem
+								onSelect={() => {
+									console.log('jet')
+									setOpen(false)
+								}}
+								className='cursor-pointer gap-x-2'
+							>
+								<PlusCircleIcon className='w-4 h-4' />
+								Add business
+							</CommandItem>
+						</CommandGroup>
+					</CommandList>
+				</Command>
+			</PopoverContent>
+		</Popover>
 	)
 }
