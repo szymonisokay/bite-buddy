1b61dbe0c7d005681c9fe5361e74af8c693b2e58
szymonisokay <szymonwalach@gmail.com>
2023-09-17 12:31:27 +0200

    styling tabs

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -13,10 +13,7 @@ const TabsList = React.forwardRef<
 >(({ className, ...props }, ref) => (
 	<TabsPrimitive.List
 		ref={ref}
-		className={cn(
-			'inline-flex h-10 items-center justify-center rounded-md bg-muted dark:bg-[#040405] p-1 text-muted-foreground',
-			className
-		)}
+		className={cn('inline-flex w-full border-b items-center', className)}
 		{...props}
 	/>
 ))
@@ -29,7 +26,7 @@ const TabsTrigger = React.forwardRef<
 	<TabsPrimitive.Trigger
 		ref={ref}
 		className={cn(
-			'inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:dark:bg-[#09090b] data-[state=active]:text-foreground data-[state=active]:shadow-sm',
+			'relative inline-flex items-center justify-center whitespace-nowrap gap-x-2 px-3 py-3 text-sm font-medium text-muted-foreground ring-offset-background transition-all hover:text-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:dark:bg-[#09090b] data-[state=active]:text-[#1a8986] dark:data-[state=active]:text-foreground data-[state=active]:shadow-sm data-[state=active]:before:absolute data-[state=active]:before:h-[2px] data-[state=active]:before:w-full data-[state=active]:before:bg-[#1a8986] data-[state=active]:before:-bottom-[1px] data-[state=active]:before:rounded-sm',
 			className
 		)}
 		{...props}
