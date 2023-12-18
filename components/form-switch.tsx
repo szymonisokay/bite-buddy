9e5112d6e689ee8aea5ddbbffc91c2e1acd0f444
szymonisokay <szymonwalach@gmail.com>
2023-12-18 22:22:42 +0100

    refactor: add optional icon and label focus

--- components/form-switch.tsx
+++ components/form-switch.tsx
@@ -1,32 +1,36 @@
 'use client'
 
-import { Label } from '@/components/ui/label'
-import { Switch } from '@/components/ui/switch'
+import { LucideIcon } from 'lucide-react'
 import { forwardRef } from 'react'
 import { ControllerRenderProps } from 'react-hook-form'
+
+import { Label } from '@/components/ui/label'
+import { Switch } from '@/components/ui/switch'
 
 type Props = {
 	label: string
 	description?: string
+	icon?: LucideIcon
 } & ControllerRenderProps
 
 export const FormSwitch = forwardRef<HTMLDivElement, Props>(
-	({ label, description, onChange, ...props }, ref) => {
+	({ label, description, icon: Icon, onChange, ...props }, ref) => {
 		return (
 			<div
 				ref={ref}
-				className='flex items-center justify-between gap-4 p-4 border rounded-md bg-muted'
+				className='flex items-center justify-between gap-4 p-4 border rounded-md'
 			>
-				<div>
-					<Label htmlFor={props.name} className='font-semibold'>
+				<Label htmlFor={props.name} className='space-y-1'>
+					<p className='flex items-center font-semibold'>
+						{Icon && <Icon className='w-4 h-4 mr-1.5' />}
 						{label}
-					</Label>
-					<p className='text-sm text-muted-foreground'>
+					</p>
+					<p className='text-sm font-normal text-muted-foreground'>
 						{description}
 					</p>
-				</div>
+				</Label>
 
-				<Switch {...props} onCheckedChange={onChange} />
+				<Switch {...props} id={props.name} onCheckedChange={onChange} />
 			</div>
 		)
 	}
